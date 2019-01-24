3f589686ee8c88349e08b2827ca327d3b17cb9c9
RobDWaller <rdwaller1984@googlemail.com>
2019-01-24 14:51:10 +0000

    Added comments to Example class

--- src/Example.php
+++ src/Example.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace App;
 
+/**
+ * This is just an example class to prove PHPUnit functions as expected.
+ *
+ * @author Rob Waller <rdwaller1984@gmail.com>
+ */
 class Example
 {
+    /**
+     * Get the word 'foo'.
+     *
+     * @return string
+     */
     public function getFoo(): string
     {
         return 'foo';
